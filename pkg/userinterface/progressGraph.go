b0f443ca14ca2aa04d48511430f7097695127753
JustinSo1 <sojustin28@gmail.com>
2021-03-09 16:03:55 -0400

    Add setters to progress graph

--- pkg/userinterface/progressGraph.go
+++ pkg/userinterface/progressGraph.go
@@ -15,13 +15,33 @@ func (graph *ProgressGraph) SetPercent(percent int) {
 	graph.Percent = percent
 }
 
+// SetTitle updates the title
+func (graph *ProgressGraph) SetTitle(title string) {
+	graph.Title = title
+}
+
+// SetBarColor updates the bar color
+func (graph *ProgressGraph) SetBarColor(barColor ui.Color) {
+	graph.BarColor = barColor
+}
+
+// SetLabelStyle updates the label style
+func (graph *ProgressGraph) SetLabelStyle(labelStyle ui.Color) {
+	graph.LabelStyle = ui.NewStyle(labelStyle)
+}
+
+// SetBorderStyleFg updates the border style
+func (graph *ProgressGraph) SetBorderStyleFg(borderStyle ui.Color) {
+	graph.BorderStyle.Fg = borderStyle
+}
+
 // NewProgressGraph returns progress graph of searching
 func NewProgressGraph() *ProgressGraph {
 	graph := &ProgressGraph{widgets.NewGauge()}
-	graph.Title = "Progress Indicator"
-	graph.Percent = 0
-	graph.BarColor = ui.ColorYellow
-	graph.LabelStyle = ui.NewStyle(ui.ColorBlue)
-	graph.BorderStyle.Fg = ui.ColorWhite
+	graph.SetTitle("Progress Indicator")
+	graph.SetPercent(0)
+	graph.SetBarColor(ui.ColorYellow)
+	graph.SetLabelStyle(ui.ColorBlue)
+	graph.SetBorderStyleFg(ui.ColorWhite)
 	return graph
 }
